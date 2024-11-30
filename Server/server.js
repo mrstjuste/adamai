0da7bf416a097e96ae283dbd970b052cb2a59e2a
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2024-11-30 13:18:28 -0500

    web commit

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+app.listen(9000, ()=> {
+    console.log('Server Started at ${9000}')
+})
+
+const mongoose = require('mongoose');
+const mongoString = "mongodb+srv://your_username:your_password@cluster0.uxnmmuy.mongodb.net/lab"
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error', (error) => console.log(error))
+
+database.once('connected', () => console.log('Databased Connected'))
